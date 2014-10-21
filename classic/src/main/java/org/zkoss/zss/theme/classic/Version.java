760bfaa7001308a0eb7843fdf690399ecac18663
jerrywell <b80203@gmail.com>
2014-10-21 07:36:18 +0100

    update version to 1.0.1

--- classic/src/main/java/org/zkoss/zss/theme/classic/Version.java
+++ classic/src/main/java/org/zkoss/zss/theme/classic/Version.java
@@ -3,5 +3,5 @@ package org.zkoss.zss.theme.classic;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "1.0.0";
+	public static final String UID = "1.0.1";
 }
